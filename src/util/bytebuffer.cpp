0cece38e6d42521397db30b19f2d2d73365d1072
Giorgio Zoppi <giorgio.zoppi@rstor.io>
2020-05-06 16:01:57 +0200

    modified:   src/util/bytebuffer.cpp 	modified:   src/util/bytebuffer.h

--- src/util/bytebuffer.cpp
+++ src/util/bytebuffer.cpp
@@ -17,3 +17,88 @@
  * under the License.
  */
 #include "bytebuffer.h"
+namespace iotdb {
+    namespace util {
+        bytebuffer::bytebuffer() {
+            _bytes.reserve(256);
+            _reader_index = 16;
+            _writer_index = 128;
+        }
+        bytebuffer::bytebuffer(size_t n) {
+            _bytes.reserve(n);
+            _reader_index = n / 16;
+            _writer_index = n / 2;
+        }
+        bytebuffer::bytebuffer(const std::initializer_list <uint8_t> &bytes) {
+            auto dim = bytes.size();
+            _bytes.reserve(dim);
+            std:
+            copy(bytes.begin(), bytes.end(), _bytes);
+        }
+        void bytebuffer::discard_bytes() {
+            _writer_index -= _reader_index;
+            _reader_index = 0;
+        }
+
+        bool bytebuffer::is_readable() const {
+            return (_reader_index <= _writer_index);
+        }
+        uint8_t bytebuffer::read() {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            auto tmp = _bytes[_reader_index];
+            _reader_index++;
+            return tmp;
+        }
+        std::vector<uint8_t> bytebuffer::read_all() {
+            if (writer_index < _reader_index) {
+                return std::vector<uint8_t>();
+            }
+            std::vector<uint8_t> array(_writer_index-_reader_index);
+            for (auto i = _reader_index; i <=writer_index; ++i)
+            {
+                array.push_back(_bytes[i]);
+            }
+            return array;
+        }
+        void bytebuffer::write(uint8_t buf) {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            if (_writer_index >= _bytes.capacity()) {
+                auto capacity = _bytes.capacity();
+                _bytes.resize(capacity * 2);
+            }
+            _bytes[_writer_index++] = buf;
+        }
+        void bytebuffer::ensure_space() {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            _bytes.reserve(_bytes.capacity() * 2);
+        }
+        size_t bytebuffer::max_writable() const {
+            return _bytes.capacity() - _writer_index;
+        }
+        const size_t bytebuffer::size() const {
+            return _bytes.size();
+        }
+        const size_t bytebuffer::capacity() const {
+            return _bytes.capacity();
+        }
+        const std::vector <uint8_t> bytebuffer::bytes() const {
+            return _bytes;
+        }
+        const std::string bytebuffer::hex() const {
+            const char code[]{"0123456789ABCDEF"};
+            std::ostringstream out;
+            for (auto &b : _bytes) {
+                auto num = b / 16;
+                auto rest = b % 16;
+                out << code[num];
+                out << code[rest];
+            }
+            return out.str();
+        }
+        bool bytebuffer::operator==(const bytebuffer::bytebuffer& bytebuffer) {
+            return hex().compare(bytebuffer.hex()) == 0;
+        }
+        uint8_t &operator[](std::size_t idx) { return _bytes[idx]; }
+        const uint8_t &operator[](std::size_t idx) const { return _bytes[idx]; }
+    }
+}
--- src/util/bytebuffer.h
+++ src/util/bytebuffer.h
@@ -21,10 +21,18 @@
 #include <vector>
 #include <cstdlib>
 #include <mutex>
+#include <utility>
 #include <string>
+#include <algorithm>
 
 namespace iotdb {
     namespace util {
+        /**
+        * bytebuffer is a thread safe class for a random and sequential accessible sequence of zero or more bytes (octets).
+        * The design is based on netty's bytebuf. We use two indexes:
+        * 1. ReaderIndex
+        * 2. WriterIndex
+        */
         class bytebuffer {
             std::vector <uint8_t> _bytes;
             size_t _reader_index = 0;
@@ -32,98 +40,98 @@ namespace iotdb {
             std::mutex _buffermutex;
             const size_t DEFAULT_SIZE = 256;
         public:
-            bytebuffer() {
-                _bytes.reserve(256);
-                _reader_index = 16;
-                _writer_index = 128;
-            }
+            /**
+             * default constructor
+             */
+            bytebuffer();
+            /**
+             * construct a byte array of predefined size
+             * @param n number of bytes that a bytearray should have
+             */
+            bytebuffer(size_t n);
+            /**
+             * construct a byte array of predefined size using uniform init
+             * @param n number of bytes that a bytearray should have
+             */
+            bytebuffer(const std::initializer_list<uint8_t>& bytes);
+            /**
+             * discard all files that has been read.
+             */
+            void discard_bytes();
+            /**
+             * Check if the buffer is readable
+             * @return true if the reader index < writer index
+             */
+            bool is_readable() const;
+            /**
+             * Read as single bute
+             * @return byte read from the buffer
+             */
+            uint8_t read();
+            /**
+             * Read all bytes
+             * @return a list of bytes
+             */
+            std::vector<uint8_t> read_all();
+            /**
+             * Read at most n bytes
+             * @param n number of bytes to read
+             * @return a tuple containing a pointer to the data and the real number of bytes read
+             */
+            std::tuple<std::unique_ptr<uint8_t>, size_t> read_n(size_t n);
 
-            bytebuffer(size_t n) {
-                _bytes.reserve(n);
-                _reader_index = n / 16;
-                _writer_index = n / 2;
-            }
-
-            void discard_bytes() {
-                _writer_index -= _reader_index;
-                _reader_index = 0;
-            }
-
-            bool is_readable() const {
-                return (_reader_index <= _writer_index);
-            }
-
-            uint8_t read() {
-                std::lock_guard <std::mutex> lock(_buffermutex);
-                auto tmp = _bytes[_reader_index];
-                _reader_index++;
-                return tmp;
-            }
-
-            void write(uint8_t buf) {
-                std::lock_guard <std::mutex> lock(_buffermutex);
-                if (_writer_index >= _bytes.capacity()) {
-                    auto capacity = _bytes.capacity();
-                    _bytes.resize(capacity * 2);
-                }
-                _bytes[_writer_index++] = buf;
-            }
-
-            void write(const uint8_t *buffer, size_t siz) {
-                std::lock_guard <std::mutex> lock(_buffermutex);
-                if (_writer_index + siz >= _bytes.capacity()) {
-                    auto capacity = _bytes.capacity() + _writer_index + siz + 1;
-                    _bytes.resize(capacity);
-                }
-                for (size_t i = 0; i < siz; ++i) {
-                    _bytes.push_back(buffer[i]);
-                    _writer_index++;
-                }
-            }
-
-            void ensure_space() {
-                std::lock_guard <std::mutex> lock(_buffermutex);
-                _bytes.reserve(_bytes.capacity() * 2);
-            }
-
-            size_t max_writable() {
-                return _bytes.capacity() - _writer_index;
-            }
-
-            const size_t size() const {
-                return _bytes.size();
-            }
-
-            const size_t capacity() const {
-                return _bytes.capacity();
-            }
-
-            const std::vector <uint8_t> bytes() const {
-                return _bytes;
-            }
-
-            const std::string hex() const {
-                const char code[]{"0123456789ABCDEF"};
-                std::ostringstream out;
-                for (auto &b : _bytes) {
-                    auto num = b / 16;
-                    auto rest = b % 16;
-                    out << code[num];
-                    out << code[rest];
-                }
-                return out.str();
-            }
-
-            bool operator==(const bytebuffer &bytebuffer) {
-                return hex().compare(bytebuffer.hex()) == 0;
-            }
-
-            uint8_t &operator[](std::size_t idx) { return _bytes[idx]; }
-
-            const uint8_t &operator[](std::size_t idx) const { return _bytes[idx]; }
-
+            /**
+             * Write a single byte
+             * @param buf byte to write
+             */
+            void write(uint8_t buf);
+            /**
+             * Write a buffer of bytes
+             * @param buffer buffer of bytes to be written
+             * @param siz    size of the array
+             */
+            void write(const uint8_t *buffer, size_t siz);
+            /**
+             * Double the reserved size for the byte buffer
+             */
+            void ensure_space();
+            /**
+             * Fetch the max number of bytes
+             * @return number of bytes
+             */
+            const size_t max_writable() const;
+            /**
+             * Get the size of the byte buffer
+             * @return size in byte of the buffer
+             */
+            const size_t size() const;
+            /**
+             * Get the real capacitty of the buffer
+             * @return
+             */
+            const size_t capacity() const;
+            /**
+             * Get the bytes converted in hexadecimal string
+             * @return hexdecimal string of the bytebuffer
+             */
+            const std::string hex() const;
+            /**
+             * Compare if two byte buffers are the same
+             */
+            bool operator==(const bytebuffer &bytebuffer);
+            /**
+             * Return the byte by random access
+             * @param idx index of the byte
+             * @return value of the byte
+             */
+            uint8_t &operator[](std::size_t idx);
+            /**
+             * Return the byte by random access
+             * @param idx index of the byte
+             * @return value of the byte
+             */
+            const uint8_t &operator[](std::size_t idx) const;
         };
-
     }
 }
 #endif //IOTDB_NATIVE_BYTEBUFFER_H
