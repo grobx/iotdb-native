a076adac73c1ba98b5bc6fb290fd3d2e0f0c3e6f
Giorgio Zoppi <giorgio.zoppi@rstor.io>
2020-05-10 01:11:52 +0200

    Added initial chunked header support 	modified:   CMakeLists.txt 	modified:   include/tsfile/file/header/chunk_header.h 	modified:   include/tsfile/file/metadata/chunk_metadata.h 	modified:   include/tsfile/file/metadata/metadata.h 	new file:   include/tsfile/file/metadata/statistics.h 	modified:   include/util/bytebuffer.h 	modified:   include/util/rwio.h 	new file:   src/CMakeLists.txt 	modified:   src/tsfile/file/header/chunk_header.cpp 	modified:   src/tsfile/file/metadata/chunk_metadata.cpp 	deleted:    src/util/bytebuffer.cpp 	modified:   test/CMakeLists.txt

--- include/tsfile/file/metadata/statistics.h
+++ include/tsfile/file/metadata/statistics.h
@@ -16,25 +16,18 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-#ifndef IOTDB_NATIVE_METADATA_H
-#define IOTDB_NATIVE_METADATA_H
+#ifndef IOTDB_NATIVE_STATISTICS_H
+#define IOTDB_NATIVE_STATISTICS_H
 namespace iotdb {
     namespace tsfile {
         namespace file {
             namespace metadata {
-                enum class compression_type {
-                    UNCOMPRESSED, SNAPPY, GZIP, LZO, SDT, PAA, PLA
-                };
-                enum class ts_datatype {
-                    BOOLEAN, INT32, INT64, FLOAT, DOUBLE, TEXT
-                };
-                enum class ts_encoding {
-                    PLAIN, PLAIN_DICTIONARY, RLE, DIFF, TS_2DIFF, BITMAP, GORILLA, REGULAR
-                };
+                namespace stats {
+                    class statistics {
+                    };
+                }
             }
         }
     }
 }
-
-#endif //IOTDB_NATIVE_METADATA_H
+#endif //IOTDB_NATIVE_STATISTICS_H
--- include/util/bytebuffer.h
+++ include/util/bytebuffer.h
@@ -167,5 +167,119 @@ namespace iotdb {
         typedef basic_bytebuffer<uint8_t> ubytebuffer;
     }
 }
-#include "../../src/util/bytebuffer.cpp"
+namespace iotdb {
+    namespace util {
+        template <typename T> basic_bytebuffer<T>::basic_bytebuffer() {
+            _bytes.reserve(256);
+            _reader_index = 16;
+            _writer_index = 128;
+        }
+        template <typename T> basic_bytebuffer<T>::basic_bytebuffer(size_t n) {
+            _bytes.reserve(n);
+            _reader_index = n / 16;
+            _writer_index = n / 2;
+        }
+        template <typename T> basic_bytebuffer<T>::basic_bytebuffer(const std::initializer_list <T> &l) {
+            auto dim = l.size();
+            _bytes.reserve(dim*2);
+            _bytes.insert(_bytes.end(), l.begin(), l.end());
+            _writer_index=dim;
+            _reader_index=0;
+        }
+        template <typename T> void basic_bytebuffer<T>::discard_bytes() {
+            _writer_index -= _reader_index;
+            _reader_index = 0;
+        }
+        template <typename T> void basic_bytebuffer<T>::read(char* s, std::streamsize n) {
+            auto data = read_n(n);
+            std::memcpy(s, data->data(), n);
+        }
+
+        template <typename T> void basic_bytebuffer<T>::get (char& c) {
+            auto v = read();
+            c = static_cast<char>(v);
+        }
+        template <typename T> bool basic_bytebuffer<T>::is_readable() const {
+            return (_reader_index <= _writer_index);
+        }
+        template <typename T> uint8_t basic_bytebuffer<T>::read() {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            auto tmp = _bytes[_reader_index];
+            _reader_index++;
+            return tmp;
+        }
+        template <typename T> std::vector<T> basic_bytebuffer<T>::read_all() {
+            if (_writer_index <= _reader_index) {
+                return std::vector<T >();
+            }
+            std::vector<T> array(_writer_index-_reader_index);
+            for (auto i = _reader_index; i <=_writer_index; ++i) {
+                array.push_back(_bytes[i]);
+            }
+            return array;
+        }
+        template <typename T> std::optional<std::vector<T>> basic_bytebuffer<T>::read_n(size_t n){
+            const size_t zero = 0;
+            if (n < zero || _writer_index - _reader_index < n) {
+                return {};
+            }
+            std::vector<T> array;
+            array.reserve(n);
+            for (auto i = _reader_index; i <_reader_index+n; ++i) {
+                array.push_back(_bytes[i]);
+            }
+            _reader_index+=n;
+            return array;
+        }
+        template <typename T> void basic_bytebuffer<T>::write(T buf) {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            if (_writer_index >= _bytes.capacity()) {
+                auto capacity = _bytes.capacity();
+                _bytes.resize(capacity * 2);
+            }
+            _writer_index+=1;
+            _bytes[_writer_index++] = buf;
+        }
+        template <typename T> void basic_bytebuffer<T>::write(const T *buffer, size_t siz) {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            _writer_index +=siz;
+            if (_writer_index >= _bytes.capacity()) {
+                _bytes.resize(_writer_index * 2);
+            }
+            for (size_t i = 0; i < siz; ++i) {
+                _bytes.push_back(buffer[i]);
+            }
+        }
+        template <typename T> void basic_bytebuffer<T>::ensure_space() {
+            std::lock_guard <std::mutex> lock(_buffermutex);
+            _bytes.reserve(_bytes.capacity() * 2);
+        }
+        template <typename T> size_t basic_bytebuffer<T>::max_writable() const {
+            return _bytes.capacity() - _writer_index;
+        }
+        template <typename T> size_t basic_bytebuffer<T>::size() const {
+            return _bytes.size();
+        }
+        template <typename T> size_t basic_bytebuffer<T>::capacity() const {
+            return _bytes.capacity();
+        }
+        template <typename T> const std::string basic_bytebuffer<T>::hex() const {
+            const char code[]{"0123456789ABCDEF"};
+            std::ostringstream out;
+            for (auto &b : _bytes) {
+                auto num = b / 16;
+                auto rest = b % 16;
+                out << code[num];
+                out << code[rest];
+            }
+            return out.str();
+        }
+        template <typename T> bool basic_bytebuffer<T>::operator==(const iotdb::util::basic_bytebuffer<T>& bytebuffer) {
+            return hex().compare(bytebuffer.hex()) == 0;
+        }
+        template <typename T>  T& basic_bytebuffer<T>::operator[](std::size_t idx) { return _bytes[idx]; }
+        template <typename T> const T&  basic_bytebuffer<T>::operator[](std::size_t idx) const { return _bytes[idx]; }
+    }
+}
+
 #endif //IOTDB_NATIVE_BYTEBUFFER_H
