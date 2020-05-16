c763aa44c9ee583d4f4ee14cf9544e5e146d37cb
Giuseppe Roberti <inbox@roberti.dev>
2020-05-16 16:07:31 +0200

    Minifix

--- include/util/bytebuffer.h
+++ include/util/bytebuffer.h
@@ -243,7 +243,7 @@ namespace iotdb {
             const T &operator[](std::size_t idx) const;
 
         private:
-            void order_byte(int8_t &b, tsfile::encoding::endian_type &order) noexcept ;
+            void order_byte(T &b, tsfile::encoding::endian_type &order) noexcept ;
 
 
         };
@@ -364,7 +364,7 @@ namespace iotdb {
         template <typename T> tsfile::encoding::endian_type basic_bytebuffer<T>::order() const noexcept {
             return _order;
         }
-        template <typename T> void basic_bytebuffer<T>::order_byte(int8_t & b,
+        template <typename T> void basic_bytebuffer<T>::order_byte(T& b,
                 tsfile::encoding::endian_type& order)  noexcept {
             const int value { 0x01 };
             const void * address = static_cast<const void *>(&value);
